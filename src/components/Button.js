f0d6477a8f9e9ed172ab9411111d63093858ec2a
TripEEE <evan.trippel@gmail.com>
2022-11-01 14:11:26 -0400

    added classes to buttons for styling and utility

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,7 +1,33 @@
 import React from "react";
 
+import classNames from "classnames"
 import "components/Button.scss";
 
 export default function Button(props) {
-   return <></>;
+  //here buttonClass uses the classNames add-on
+  //"button" is always a className
+  //"button--confirm" is only a className if props.confirm is truthy
+  //"button--danger" is only a className if props.danger is truthy
+
+  const buttonClass = classNames("button", {
+    "button--confirm": props.confirm,
+    "button--danger": props.danger
+  })
+
+  // //all buttons have a class of button via buttonClass
+  // let buttonClass = "button"
+
+  // //if props.confirm is true on a Button component,
+  // //add button--confirm to that class
+  // if (props.confirm) {
+  //   buttonClass += " button--confirm"
+  // }
+
+  // if (props.danger) {
+  //   buttonClass += " button--danger"
+  // }
+
+  return (
+    <button className={buttonClass} onClick={props.onClick} disabled={props.disabled}>{props.children}</button>
+  );
 }
